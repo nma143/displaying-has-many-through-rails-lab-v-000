c56c0a03529e52ded8c95d3a66efb6e2acb00603
Nikki A <nikki.apostolakis@gmail.com>
2019-01-30 19:17:37 +0000

    Automatically backed up by Learn

--- db/migrate/20190130190740_create_appointments.rb
+++ db/migrate/20190130190740_create_appointments.rb
@@ -2,6 +2,8 @@ class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
       t.datetime  :datetime
+      t.belongs_to  :doctor
+      t.belongs_to  :patient
       t.timestamps null: false
     end
   end
